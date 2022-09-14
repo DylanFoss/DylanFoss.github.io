76077c76d4f496dbe9aa2985d0d443b650ecdf80
DylanFoss <57671477+DylanFoss@users.noreply.github.com>
2022-09-14 13:37:23 +0100

    Html,css,js

---
+++ assets/js/projects.js
@@ -0,0 +1,27 @@
+var githubUsername = 'DylanFoss'
+
+function updateProjects(){
+  fetch('https://gh-pinned-repos.egoist.sh/?username=' + githubUsername)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(addProject);
+  });
+
+}
+
+function addProject(project, _){
+  if (project['name'] != githubUsername + '.github.io'){
+    if ('content' in document.createElement('template')) {
+      var projects = document.getElementById('project-cards');
+      var template = document.getElementById('project-card-template');
+
+      var card = template.content.cloneNode(true);
+      console.log(card.querySelector("div"));
+      card.querySelector("h5").innerHTML = project['repo'];
+      card.querySelector("p").innerHTML = project['description'];
+      card.querySelector("a").href = project['html_url'];
+
+      projects.appendChild(card);
+    }
+  }
+}
